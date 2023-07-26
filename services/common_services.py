73ba27383c1a0e7964b928cedf27afad6ec42809
idefeng <2921558948@qq.com>
2023-07-26 12:21:12 +0800

    完善通用服务类中区分文件类型的种类

--- services/common_services.py
+++ services/common_services.py
@@ -10,14 +10,20 @@ def verify_file_type(filename) -> int:
     file_type = filename.split('.')
     if file_type[-1].upper() in ('EXE', 'MSI'):
         return 1
-    elif file_type[-1].upper() in ('ZIP', 'RAR'):
+    elif file_type[-1].upper() in ('ZIP', 'RAR', 'GZ'):
         return 2
-    elif file_type[-1].upper() in ('DOC', 'DOCX', 'XLS', 'XLSX', 'PPT', 'PPTX', 'TXT'):
+    elif file_type[-1].upper() in ('DOC', 'DOCX', 'XLS', 'XLSX', 'PPT', 'PPTX', 'TXT', 'PDF'):
         return 3
-    elif file_type[-1].upper() in 'MP3':
+    elif file_type[-1].upper() in ('MP3', 'MID', 'WAV', 'AU', 'RAM', 'WMA', 'MMF', 'AMR', 'AAC', 'FLAC'):
         return 4
-    elif file_type[-1].upper() in ('MP4', 'AVI', 'WMV'):
+    elif file_type[-1].upper() in ('MP4', 'AVI', 'WMV', 'RM', 'MPG', 'MOV', 'SWF'):
         return 5
-    else:
+    elif file_type[-1].upper() in ('BMP', 'GIF', 'JPG', 'JPEG', 'PIC', 'PNG', 'TIF'):
         return 6
+    else:
+        return 7
 
+
+if __name__ == '__main__':
+    filename = "a.doc.zip"
+    print(verify_file_type(filename))
